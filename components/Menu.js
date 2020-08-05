9cf5f5b7e685e6c68282880cc3d1bd697343d29b
Nicole Tilbe <nicolecali2406@outlook.com>
2020-08-05 17:57:00 -0400

    finished menu.js and stretch

--- components/Menu.js
+++ components/Menu.js
@@ -31,3 +31,36 @@ let menuItems = [
 
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
+
+// Step 1 (Part 1) - created a function component called 'menuMaker'
+function menuMaker(menuArr) {
+  // Step 1 (Part 2) - created the menu 
+  const menu = document.createElement('div')
+  const menuList = document.createElement('ul')
+
+  // appended menuList to the menu variable and gave it a class called 'menu'
+  menu.appendChild(menuList)
+  menu.classList.add('menu')
+
+  // Step 2 - using a forEach to iterate over the array creating a list item element
+  // for each item in the array. And then append those items to the 'menuList'.
+  menuArr.forEach(name => {
+    const listItem = document.createElement('li')
+    listItem.textContent = name
+    menuList.appendChild(listItem)
+  })
+
+  // Step 3 - created the menu button
+  const button = document.querySelector('.menu-button')
+
+  // Step 4 - added a click event listener to the menu button. When clicked it toggles the class
+  // 'menu--open' on div.menu (your div with a 'menu' class).
+  button.addEventListener('click', () => document.querySelector('.menu').classList.toggle('menu--open'))
+  
+  // Step 5 - returning the div 'menu'
+  return menu
+}
+
+// Step 6 - Using menuMaker, created a menu using the 'menuItems' array
+// and append the returned menu to the header
+document.querySelector('.header').appendChild(menuMaker(menuItems))
